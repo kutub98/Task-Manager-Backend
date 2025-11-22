93df3718899074e033017ea894c883ff638c7465
Kutub Udd <socialnetwork98710@gmail.com>
2025-11-22 11:23:26 +0600

    added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 // const connectDB = async () => {
 //   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
+//     await mongoose.connect(process.env.MONGODB_URI, {
 //       useNewUrlParser: true,
 //       useUnifiedTopology: true,
 //     });
@@ -28,7 +28,7 @@ async function connectDB() {
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(process.env.MONGO_URI, {
+      .connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
